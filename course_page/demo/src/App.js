5779e61a57a761b3a01ec98d053178a4d378f325
Harshavardhan Bajoria <hvbajoria@hotmail.com>
2023-07-03 00:49:56 +0530

    Files added

---
+++ course_page/demo/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Page from './components/course1';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Page />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
